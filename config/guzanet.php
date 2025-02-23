4261e47326644b1d3c706e68b3e71fec6e9c8783
Ramuel Gonzalez <ramuelcl@gmail.com>
2025-02-23 16:48:55 +0100

    main - logo corregido

---
+++ config/guzanet.php
@@ -0,0 +1,49 @@
+<?php
+
+// config/guzanet.php
+return [
+  "sistema" => [
+    ["banca" => 1],
+    ["trabajo" => 2],
+    ["ventas" => 3],
+    ["compras" => 4],
+    ["inventario" => 5],
+    ["remuneraciones" => 7],
+  ],
+  "clave" => "valor",
+  "appEmpresa" => "Guzanet",
+  "appLogo" => "images/Guzanet.png", //public\app\logo
+
+  "appNombre" => "Ramuel Gonzalez",
+  "appMail" => "Ramuelcl@gmail.com",
+  "appLargoClave" => 3,
+
+  "idioma" => "es-ES",
+  "idiomasDir" => "app/flags/",
+  "idiomas" => ["es" => "Español", "en" => "English", "fr" => "Français"],
+  "icon_paths" => [
+    "solid" => "app/icons/solid", //public\app\icons
+    "outline" => "app/icons/outline", //public\app\icons
+  ],
+  "appThemes" => [
+    "colores" => ["azul", "verde", "amarillo", "rojo"],
+    "fondo" => "images/app/fondo.png",
+  ],
+  "appTheme" => "theme01",
+];
+
+/**
+ *
+// Acceder a un valor específico
+$valor = config('nombre-del-archivo.clave');
+
+// Acceder a todos los valores como un array asociativo
+$configuracion = config('nombre-del-archivo');
+
+// Obtener el valor actual de la configuración
+$valorActual = config('nombre-del-archivo.clave');
+
+// Modificar el valor en tiempo de ejecución
+config(['nombre-del-archivo.clave' => 'nuevo_valor']);
+
+ */
--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,6 +1,24 @@
-<a href="/">
-    <svg class="size-16" viewbox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
-        <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
-    </svg>
+<!-- resources/views/components/authentication-card-logo.blade.php -->
+@props(['size' => 'mini', 'url' => '#'])
+
+@php
+  // Validar y establecer el tamaño
+  $size = in_array($size, ['mini', 'med', 'large']) ? $size : 'mini';
+
+  // Establecer el tamaño predeterminado según el valor de $size
+  $defaultSize = match ($size) {
+      'mini' => 50,
+      'med' => 100,
+      'large' => 200,
+  };
+
+  // Establecer la URL predeterminada si no se proporciona una
+  $url = $url === '#' ? url('/') : $url;
+
+  // Obtener la ruta de la imagen desde la configuración o usar una predeterminada
+  $img = config('guzanet.appLogo', 'images/guzanet.png');
+@endphp
+
+<a href="{{ $url }}">
+  <img src="{{ asset($img) }}" alt="Logo" style="width: {{ $defaultSize }}px; height: {{ $defaultSize }}px;">
 </a>
